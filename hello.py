689b841c536ef4bef960969d5226cebadddf4e6a
Croccodoyle <mikey_doyle@me.com>
2024-04-03 19:19:19 +0100

    balls

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from lib import run_mike  # Replace my_function with the actual function name
 
-st.write ('hello')
+st.write('Hello')
+
+if st.button('Click me'):
+    result = run_mike()
+    st.write(result)
